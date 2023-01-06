7b7b49a4e4a38af287219a09ca7bff375315c3c7
yoshinorin <yoshinorin.net@outlook.com>
2023-01-06 20:24:15 +0900

    add dependencies

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { print1 } from '../../monorepo-sandbox-sub1/src/index.js'
+import { print1, print2 } from '@yoshinorin/monorepo-sandbox-sub1/src/index.js'
 
 console.log("this is a main repo")
 print1()
+print2()
